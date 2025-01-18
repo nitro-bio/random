362301328fc76ceb7299a508417d79966edb0264
Nishant Jha <github@nishantjha.org>
2025-01-18 12:25:27 -0800

    [wip]

---
+++ app/PdbSequence/MoleculeSection.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { useDebounce } from "@/hooks/useDebounce";
+import { cn } from "@/lib/utils";
+import { AriadneSelection } from "@nitro-bio/sequence-viewers";
+import { Loader2Icon, XIcon } from "lucide-react";
+import dynamic from "next/dynamic";
+import { memo, useMemo } from "react";
+const MoleculeViewer = dynamic(
+  async () => {
+    const m = await import("@nitro-bio/molstar-easy");
+    return m.MoleculeViewer;
+  },
+  {
+    ssr: false,
+    loading: () => (
+      <div className={cn("flex h-80 items-center justify-center")}>
+        <Loader2Icon className="h-4 w-4 animate-spin" />
+      </div>
+    ),
+  },
+);
+const MoleculeSection = memo(
+  ({
+    pdbString,
+    error,
+    isFetching,
+    selection,
+    className,
+  }: {
+    pdbString?: string | null;
+    error?: Error;
+    isFetching?: boolean;
+    selection: AriadneSelection | null;
+    className?: string;
+  }) => {
+    const debouncedSelection = useDebounce<AriadneSelection | null>({
+      value: selection,
+      delay: 500,
+    });
+
+    const selectionHighlights = useMemo(() => {
+      if (!debouncedSelection) return [];
+      const { start, end } = debouncedSelection;
+      // TODO: handle selection over seam
+      return [
+        {
+          start,
+          end,
+          label: {
+            text: "⠀", // hides label
+            hexColor: "#0ea5e9", // sky-500
+          },
+        },
+      ];
+    }, [debouncedSelection]);
+
+    const moleculePayloads = useMemo(() => {
+      return [
+        {
+          pdbString: pdbString ?? "",
+          highlights: selectionHighlights,
+	  structureHexColor: "#f4f4f5", // zinc-100
+        },
+      ];
+    }, [pdbString, selectionHighlights]);
+
+    const Wrapper = ({ children }: { children: React.ReactNode }) => (
+      <div
+        className={cn(
+          className,
+          "flex h-80 items-center justify-center text-red-400",
+        )}
+      >
+        {children}
+      </div>
+    );
+    if (error) {
+      return (
+        <Wrapper>
+          <XIcon className="h-4 w-4 text-red-400" />
+          <span className="ml-2">Failed to fetch PDB</span>
+        </Wrapper>
+      );
+    }
+    if (isFetching) {
+      return (
+        <Wrapper>
+          <Loader2Icon className="h-4 w-4 animate-spin" />
+        </Wrapper>
+      );
+    }
+    if (!pdbString) {
+      return (
+        <Wrapper>
+          <span>No PDB found</span>
+        </Wrapper>
+      );
+    }
+    return (
+      <Wrapper>
+        <MoleculeViewer
+          moleculePayloads={moleculePayloads}
+          // bg-zinc-900
+          backgroundHexColor="#18181b"
+
+          className="h-80"
+        />
+      </Wrapper>
+    );
+  },
+);
+MoleculeSection.displayName = "MoleculeSection";
+export default MoleculeSection;
--- app/PdbSequence/PdbEditor.tsx
+++ app/PdbSequence/PdbEditor.tsx
@@ -1,47 +1,45 @@
 "use client";
 import { Separator } from "@/components/ui/separator";
 
-import { useQuery } from "@tanstack/react-query";
-import { Loader2Icon, RotateCcwIcon, XIcon } from "lucide-react";
-import dynamic from "next/dynamic";
-import { useEffect, useMemo, useState } from "react";
-import { getPDBString, INITIAL_PDB_ID, pdbToSequence } from "./utils";
-import { useDebounce } from "@/hooks/useDebounce";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useForm } from "react-hook-form";
-import { AriadneSelection, SequenceViewer } from "@nitro-bio/sequence-viewers";
-import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
+import { useDebounce } from "@/hooks/useDebounce";
 import { useStickyState } from "@/hooks/useStickyState";
-import { AMINO_ACIDS, NUCLEOTIDES, PUNCTUATION } from "@/lib/constants";
-import { BaseSelector } from "@/components/BaseSelector";
-import { Button } from "@/components/ui/button";
-const MoleculeViewer = dynamic(
-  async () => {
-    const m = await import("@nitro-bio/molstar-easy");
-    return m.MoleculeViewer;
-  },
-  {
-    ssr: false,
-    loading: () => (
-      <div className={cn("flex min-h-80 items-center justify-center")}>
-        <Loader2Icon className="h-4 w-4 animate-spin" />
-      </div>
-    ),
-  },
-);
+import { cn } from "@/lib/utils";
+import { AriadneSelection } from "@nitro-bio/sequence-viewers";
+import { useQuery } from "@tanstack/react-query";
+import { RotateCcwIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { getPDBString, INITIAL_PDB_ID, pdbToSequence } from "./utils";
+import MoleculeSection from "./MoleculeSection";
+import SequenceSection from "./SequenceSection";
 
 export const PDBEditor = () => {
-  const { register, setValue, getValues, watch } = useForm({
+  const [initialPdbId, setInitialPdbId] = useStickyState({
+    defaultValue: INITIAL_PDB_ID,
+    prefix: "nitro-pdb",
+    key: "pdbId",
+    version: "0",
+  });
+  const { register, setValue, watch } = useForm({
     defaultValues: {
-      pdbId: INITIAL_PDB_ID,
+      pdbId: initialPdbId,
       sequence: "",
     },
   });
   const pdbId = watch("pdbId");
+  useEffect(
+    function _setInitialPdbId() {
+      setInitialPdbId(pdbId);
+    },
+    [pdbId],
+  );
   const sequence = watch("sequence");
-  const setSequence = (value: string) => setValue("sequence", value);
+  const setSequence = (value: string) =>
+    setValue("sequence", value.toUpperCase());
   const debouncedPdbId = useDebounce({ value: pdbId, delay: 200 });
   const {
     data: pdbString,
@@ -51,34 +49,38 @@ export const PDBEditor = () => {
     queryKey: ["pdbString", debouncedPdbId],
     queryFn: () => getPDBString(debouncedPdbId),
   });
+  const sequenceFromPdb = pdbString && pdbToSequence(pdbString);
+
   useEffect(
     function updateSequenceFromPdbString() {
-      if (!pdbString) return;
-      const sequence = pdbToSequence(pdbString);
-      setSequence(sequence);
+      if (!sequenceFromPdb) return;
+      setSequence(sequenceFromPdb);
     },
-    [pdbString],
+    [sequenceFromPdb],
   );
+  const showReset = sequenceFromPdb && sequence !== sequenceFromPdb;
+
   const [selection, setSelection] = useState<AriadneSelection | null>(null);
-  const showReset = pdbString && sequence !== pdbToSequence(pdbString);
+
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid w-full grid-cols-3 gap-4">
       <div className="flex flex-col gap-2">
         <Label htmlFor="pdbId">PDB Id</Label>
         <Input
-          className="w-fit min-w-[180px]"
+          className="w-fit min-w-[180px] uppercase"
           type="text"
           placeholder="Enter PDB Id"
           {...register("pdbId")}
         />
       </div>
-      <MoleculeViewerViaPdbId
-        pdbString={pdbString}
-        selection={selection}
-        isFetching={isFetching}
-        error={error as Error}
-        className="col-span-2 -mt-16 overflow-hidden rounded-md border"
-      />
+      <div className="col-span-2 -mt-16 overflow-hidden rounded-md border">
+        <MoleculeSection
+          pdbString={pdbString}
+          selection={selection}
+          isFetching={isFetching}
+          error={error as Error}
+        />
+      </div>
       <Separator className="col-span-3 my-8" />
       <>
         <div className="flex flex-col">
@@ -109,161 +111,17 @@ export const PDBEditor = () => {
             </span>
           </Label>
           <Textarea
-            className="whitespace-pre-line"
+            className="whitespace-pre-line uppercase"
             {...register("sequence", {})}
           />
         </div>
-        <SequenceViewerViaPdbId
-          sequence={sequence}
+        <SequenceSection
+          sequence={sequence.toUpperCase()}
           selection={selection}
           setSelection={setSelection}
           className="col-span-2"
         />
       </>
-    </div>
-  );
-};
-
-export const SequenceViewerViaPdbId = ({
-  sequence,
-  selection,
-  setSelection,
-  className,
-}: {
-  sequence?: string;
-  selection: AriadneSelection | null;
-  setSelection: (selection: AriadneSelection | null) => void;
-  className?: string;
-}) => {
-  // initial map where all amino acids are true, rest are false
-  const [baseMap, setBaseMap] = useStickyState<Record<string, boolean>>({
-    defaultValue: Object.fromEntries(
-      [
-        PUNCTUATION.map((p) => [p, false]),
-        NUCLEOTIDES.map((n) => [n, false]),
-        AMINO_ACIDS.map((a) => [a, true]),
-      ].flat(),
-    ),
-    prefix: "nitro-pdb",
-    key: "baseMap",
-    version: "0",
-  });
-  const allowedBases = Object.keys(baseMap).filter((key) => baseMap[key]);
-
-  const charClassName = ({ base }: { base: { base: string } }) => {
-    const classNames = [
-      "hover:bg-zinc-800/80 hover:outline hover:outline-[0.25px] hover:outline-zinc-400",
-      "hover:scale-[200%]",
-    ];
-    if (!allowedBases.includes(base.base)) {
-      classNames.push("text-red-400 underline");
-    } else {
-      classNames.push("text-zinc-100");
-    }
-    return cn(...classNames);
-  };
-
-  return (
-    <div className={cn(className)}>
-      <BaseSelector
-        baseMap={baseMap}
-        setBaseMap={setBaseMap}
-        className="-mb-2 -mt-2"
-      />
-      <SequenceViewer
-        sequences={[sequence ?? ""]}
-        annotations={[]}
-        selection={selection}
-        setSelection={setSelection}
-        charClassName={charClassName}
-        selectionClassName="relative after:bg-blue-400/30 after:absolute after:-left-px after:right-0 after:inset-y-0 after:z-[-1]"
-        containerClassName="max-h-[600px] overflow-y-auto h-fit"
-      />
-    </div>
-  );
-};
-
-export const MoleculeViewerViaPdbId = ({
-  pdbString,
-  error,
-  isFetching,
-  selection,
-  className,
-}: {
-  pdbString?: string | null;
-  error?: Error;
-  isFetching?: boolean;
-  selection: AriadneSelection | null;
-  className?: string;
-}) => {
-  const debouncedSelection = useDebounce<AriadneSelection | null>({
-    value: selection,
-    delay: 500,
-  });
-
-  const selectionHighlights = useMemo(() => {
-    if (!debouncedSelection) return [];
-    const { start, end } = debouncedSelection;
-    // TODO: handle selection over seam
-    return [
-      {
-        start,
-        end,
-        label: {
-          text: "⠀", // hides label
-          hexColor: "#93c5fd", // blue-300
-        },
-      },
-    ];
-  }, [debouncedSelection]);
-
-  const moleculePayloads = useMemo(() => {
-    return [
-      {
-        pdbString: pdbString ?? "",
-        highlights: selectionHighlights,
-      },
-    ];
-  }, [pdbString, selectionHighlights]);
-
-  if (error) {
-    return (
-      <div
-        className={cn(
-          className,
-          "flex min-h-80 items-center justify-center text-red-400",
-        )}
-      >
-        <XIcon className="h-4 w-4 text-red-400" />
-        <span className="ml-2">Failed to fetch PDB</span>
-      </div>
-    );
-  }
-  if (isFetching) {
-    return (
-      <div
-        className={cn(className, "flex min-h-80 items-center justify-center")}
-      >
-        <Loader2Icon className="h-4 w-4 animate-spin" />
-      </div>
-    );
-  }
-  if (!pdbString) {
-    return (
-      <div
-        className={cn(className, "flex min-h-80 items-center justify-center")}
-      >
-        <span>No PDB found</span>
-      </div>
-    );
-  }
-  return (
-    <div className={cn(className, "min-h-80")}>
-      <MoleculeViewer
-        moleculePayloads={moleculePayloads}
-        // bg-zinc-900
-        backgroundHexColor="#18181b"
-      />
     </div>
   );
 };
